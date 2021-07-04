65674ebb4b63617e287cf9bb2a09bc3e0c69c684
YasinVeliyev <veliyev.yasin@gmail.com>
2021-07-04 23:04:04 +0400

    modified:   Express/app.js 	new file:   Express/content/index.html 	new file:   Express/public/index.js 	new file:   Express/public/logo.svg 	new file:   Express/public/style.css

--- Express/app.js
+++ Express/app.js
@@ -1,16 +1,16 @@
 const express = require('express')
-
+const path = require('path')
 app = express()
 
+//setup static and middleware
+app.use(express.static('./public'))
 
-
-app.get("/", (req, res)=>{
-    console.log('User hit the reseource')
-    res.send("Hompe Page")
-})
-app.all("*", (req, res)=>{
-    res.status(404).send('<h1>Not Found</h1>')
+app.get("/", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './content/index.html'))
 })
 
+app.all("*", (req, res) => {
+    res.status(404).send('Reseource not found')
+})
 
 app.listen(5000, ()=>console.log('Server is running'))
