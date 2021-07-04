1d7cc6a5f7fa41e3e08e6d6b39c45838b81ab1d5
YasinVeliyev <veliyev.yasin@gmail.com>
2021-07-05 00:46:44 +0400

    modified:   Express/app.js 	new file:   Express/data.js

--- Express/app.js
+++ Express/app.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const path = require('path')
+const {products} = require('./data')
+
+
 app = express()
 
 //setup static and middleware
 app.use(express.static('./public'))
-app.use(express.static('./content'))
-// app.get("/", (req, res) =>{
-//     res.sendFile(path.resolve(__dirname, './content/index.html'))
-// })
+// app.use(express.static('./content'))
+app.get("/", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './content/index.html'))
+})
+
+app.get('/api/', (req, res)=>{
+    res.json(products)
+})
 
 app.all("*", (req, res) => {
     res.status(404).send('Reseource not found')
