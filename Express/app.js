cb99b86d0db0c1feac02da7601c4d7c632283a4d
YasinVeliyev <veliyev.yasin@gmail.com>
2021-07-04 20:54:57 +0400

    modified:   Express/app.js

--- Express/app.js
+++ Express/app.js
@@ -2,8 +2,15 @@ const express = require('express')
 
 app = express()
 
+
+
 app.get("/", (req, res)=>{
+    console.log('User hit the reseource')
     res.send("Hompe Page")
 })
+app.all("*", (req, res)=>{
+    res.status(404).send('<h1>Not Found</h1>')
+})
+
 
 app.listen(5000, ()=>console.log('Server is running'))
